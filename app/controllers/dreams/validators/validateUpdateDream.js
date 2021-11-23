33e941ba85fb1ff78786a79c209fa64482aabf67
Kamil Pinas <kamilpinas@gmail.com>
2021-11-23 22:54:58 +0100

    +

--- app/controllers/dreams/validators/validateUpdateDream.js
+++ app/controllers/dreams/validators/validateUpdateDream.js
@@ -4,22 +4,46 @@ const { check } = require('express-validator')
 /**
  * Validates update item request
  */
-const validateUpdateCity = [
-  check('title')
-    .exists()
-    .withMessage('MISSING')
-    .not()
-    .isEmpty()
-    .withMessage('IS_EMPTY'),
+const validateUpdateDream = [
   check('id')
     .exists()
     .withMessage('MISSING')
     .not()
     .isEmpty()
     .withMessage('IS_EMPTY'),
+  check('title')
+    .exists()
+    .withMessage('MISSING')
+    .not()
+    .isEmpty()
+    .withMessage('IS_EMPTY')
+    .trim(),
+  check('description')
+    .exists()
+    .withMessage('MISSING')
+    .not()
+    .isEmpty()
+    .withMessage('IS_EMPTY')
+    .trim(),
+  check('startDate')
+    .exists()
+    .withMessage('MISSING')
+    .not()
+    .isEmpty()
+    .withMessage('IS_EMPTY')
+    .trim(),
+  check('endDate')
+    .exists()
+    .withMessage('MISSING')
+    .not()
+    .isEmpty()
+    .withMessage('IS_EMPTY')
+    .trim(),
+  check('category').exists().withMessage('MISSING'),
+  check('analysis').exists().withMessage('MISSING'),
   (req, res, next) => {
     validateResult(req, res, next)
   }
 ]
 
-module.exports = { validateUpdateCity }
+module.exports = { validateUpdateDream }
